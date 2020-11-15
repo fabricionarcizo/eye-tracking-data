b392bc96233fe7cd2c183427397fc4c46821ef9d
Fabricio Batista Narcizo <fabn@itu.dk>
2020-11-15 06:48:17 +0100

    Generate the gaze-error bar plot

--- 03_scripts/00_python/create_error_bar.py
+++ 03_scripts/00_python/create_error_bar.py
@@ -1,35 +1,82 @@
+# Imported libraries.
+import glob
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+from scipy import signal
+from lmfit.models import GaussianModel
 
-def gaussian(x, height, center, width, offset):
-    return height * np.exp(-(x - center) ** 2 / (2 * width ** 2)) + offset
+# Exponential and Gaussian model.
+gauss1Model = GaussianModel(prefix="g1_")
+gauss2Model = GaussianModel(prefix="g2_")
 
-files = [ "../../01_simulated/03_final_results/00_interpolate_00_original.csv",
-          "../../01_simulated/03_final_results/00_interpolate_01_camera.csv",
-          "../../01_simulated/03_final_results/00_interpolate_02_distortion.csv",
-          "../../01_simulated/03_final_results/01_homography_00_original.csv",
-          "../../01_simulated/03_final_results/01_homography_01_camera.csv",
-          "../../01_simulated/03_final_results/01_homography_02_distortion.csv" ]
+# Input CSV files.
+files = [ "../../01_simulated/02_data_analysis/00_interpolate/00_original/*.csv",
+          "../../01_simulated/02_data_analysis/00_interpolate/01_camera/*.csv",
+          "../../01_simulated/02_data_analysis/00_interpolate/02_distortion/*.csv",
+          "../../01_simulated/02_data_analysis/01_homography/00_original/*.csv",
+          "../../01_simulated/02_data_analysis/01_homography/01_camera/*.csv",
+          "../../01_simulated/02_data_analysis/01_homography/02_distortion/*.csv" ]
 
-titles = ["Polynomial (Original)", "Polynomial (Camera)", "Polynomial (Distortion)",
-          "Homography (Original)", "Homography (Camera)", "Homography (Distortion)"]
+titles = [ "Polynomial (Original)", "Polynomial (Camera)", "Polynomial (Distortion)",
+           "Homography (Original)", "Homography (Camera)", "Homography (Distortion)" ]
 
-for i, file in enumerate(files):
-        plt.subplot(2, 3, i + 1)
-        data = pd.read_csv(file)
+# Process each experiment individually.
+for i, path in enumerate(files):
+    plt.subplot(2, 3, i + 1)
 
-        x = np.linspace(-200, 200, 21)
-        y = data.groupby("camera_pos_x").error_deg_xyz.apply(lambda c: c.abs().mean()).values
-        yerr = data.groupby("camera_pos_x").error_deg_xyz.apply(lambda c: c.abs().std()).values
+    # Collect the eye-tracking data of each experiment.
+    csvs = glob.glob(path)
+    csvs.sort()
 
-        container = plt.bar(x, y, width=20, color="0.5", alpha=0.5, edgecolor="k", yerr=yerr, capsize=3)
+    errorX = []
+    errorY = []
+    for csv in csvs:
+        data = pd.read_csv(csv)
+        errorX.append(data.error_px_x.values)
+        errorY.append(data.error_px_y.values)
+    errorX = np.asarray(errorX).reshape(-1)
+    errorY = np.asarray(errorY).reshape(-1)
 
-        p1, cov_matrix = curve_fit(gaussian, x, y)
-        x_plot = np.linspace(-210, 210, 1000)
-        plt.plot(x_plot, gaussian(x_plot, *p1), 'r-', lw=2)
+    # Plot the X-axis error.
+    hist, bins = np.histogram(errorX, bins=21, density=True)
+    unityHist = hist / hist.sum()
+    error = np.sqrt(hist) / hist.sum()
+    widths = bins[:-1] - bins[1:]
 
-        plt.title(titles[i])
+    # Calculate the Gaussian parameters.
+    peaks, properties = signal.find_peaks(unityHist, width=True)
+
+    if len(peaks) > 1:
+        model = gauss1Model + gauss2Model
+        p1, p2, p3 = np.unique((properties["left_bases"], properties["right_bases"]))
+        params1 = gauss1Model.guess(unityHist[p1:p2], x=bins[p1:p2])
+        params2 = gauss2Model.guess(unityHist[p2:p3], x=bins[p2:p3])
+        params = params1.update(params2)
+    else:
+        model = gauss1Model
+        params = model.guess(unityHist, x=bins[1:])
+
+    # Fit the Gaussian curve.
+    print(i, bins)
+    result = model.fit(unityHist, params, x=bins[1:])
+    xPlot = np.linspace(bins.min() * 1.10, bins.max() * 1.10, 1000)
+    comps = result.eval_components(x=xPlot)
+
+    # Plot the histogram.
+    plt.bar(bins[1:], unityHist, width=widths, edgecolor="k", alpha=0.5, color="k",
+            yerr=error, error_kw=dict(lw=1, capsize=2, capthick=1))
+    # plt.axvline(xPlot.mean(), color="k", alpha=0.5, linestyle='dashed', linewidth=1)
+
+    plt.plot(xPlot, comps["g1_"], "k-", alpha=0.5, lw=2)
+    if len(peaks) > 1:
+        plt.plot(xPlot, comps["g2_"], "k-", alpha=0.5, lw=2)
+
+    # Plot information.
+    plt.title(titles[i])
+    if i == 0 or i == 3:
+        plt.ylabel("Normalized Density")
+    if i > 2:
+        plt.xlabel("Horizontal Gaze Error (metros)")
 
 plt.show()
--- 03_scripts/00_python/create_simulated_data_output.py
+++ 03_scripts/00_python/create_simulated_data_output.py
@@ -26,7 +26,7 @@ for input, output, stat in zip(inputs, outputs, stats):
     files = glob.glob(input)
     files.sort()
 
-    errors = np.zeros((len(files), 8))
+    errors = np.zeros((len(files), 11))
 
     for i, file in enumerate(files):
         print("%s: %d" % (input, i))
@@ -36,36 +36,50 @@ for input, output, stat in zip(inputs, outputs, stats):
         errors[i, 2] = int(file.split("/")[-1].split("_")[2][1:])
         errors[i, 3] = int(file.split("/")[-1].split("_")[3][1:])
 
-        errors[i, 4] = abs(data["error_deg_x"].values).mean()
-        errors[i, 5] = abs(data["error_deg_y"].values).mean()
-        errors[i, 6] = abs(data["error_deg_z"].values).mean()
-        errors[i, 7] = data["error_deg_xyz"].values.mean()
+        errors[i, 4] = (data["error_px_x"].values).mean()
+        errors[i, 5] = (data["error_px_y"].values).mean()
+        errors[i, 6] = (data["error_px_xy"].values).mean()
+
+        errors[i,  7] = (data["error_deg_x"].values).mean()
+        errors[i,  8] = (data["error_deg_y"].values).mean()
+        errors[i,  9] = (data["error_deg_z"].values).mean()
+        errors[i, 10] = (data["error_deg_xyz"].values).mean()
 
     errors = errors[~np.all(errors[:, 1:4] == 0, axis=1)]
 
     sts = np.array([[
-        errors[:, 4].mean(),
-        errors[:, 4].std(),
-        errors[:, 5].mean(),
-        errors[:, 5].std(),
-        errors[:, 6].mean(),
-        errors[:, 6].std(),
-        errors[:, 7].mean(),
-        errors[:, 7].std()
+        errors[:,  4].mean(),
+        errors[:,  4].std(),
+        errors[:,  5].mean(),
+        errors[:,  5].std(),
+        errors[:,  6].mean(),
+        errors[:,  6].std(),
+        errors[:,  7].mean(),
+        errors[:,  7].std(),
+        errors[:,  8].mean(),
+        errors[:,  8].std(),
+        errors[:,  9].mean(),
+        errors[:,  9].std(),
+        errors[:, 10].mean(),
+        errors[:, 10].std()
     ]])
 
-    mean = errors[:, 4:6].mean(axis=0)
-    std = np.array([sts[0, 1], sts[0, 3]])
-    errors = errors[abs(errors[:, 4] - mean[0]) < std[0] * 3]
-    errors = errors[abs(errors[:, 5] - mean[1]) < std[1] * 3]
+    mean = errors[:, 7:9].mean(axis=0)
+    std = np.array([sts[0, 7], sts[0, 9]])
+    errors = errors[(errors[:, 7] - mean[0]) < std[0] * 3]
+    errors = errors[(errors[:, 8] - mean[1]) < std[1] * 3]
 
     df = pd.DataFrame(errors, columns=[ "experiment", "camera_pos_x", "camera_pos_y", "camera_pos_z",
+                                        "error_px_x", "error_px_y", "error_px_xy",
                                         "error_deg_x", "error_deg_y", "error_deg_z", "error_deg_xyz" ])
     df.experiment = df.experiment.astype(int)
     df.to_csv(output, sep=",", index=False)
 
-    df = pd.DataFrame(sts, columns=["error_deg_x_mean", "error_deg_x_std",
-                                    "error_deg_y_mean", "error_deg_y_std",
-                                    "error_deg_z_mean", "error_deg_z_std",
-                                    "error_deg_xyz_mean", "error_deg_xyz_std"])
+    df = pd.DataFrame(sts, columns=[ "error_px_x_mean", "error_px_x_std",
+                                     "error_px_y_mean", "error_px_y_std",
+                                     "error_px_xy_mean", "error_px_xy_std",
+                                     "error_deg_x_mean", "error_deg_x_std",
+                                     "error_deg_y_mean", "error_deg_y_std",
+                                     "error_deg_z_mean", "error_deg_z_std",
+                                     "error_deg_xyz_mean", "error_deg_xyz_std" ])
     df.to_csv(stat, sep=",", index=False)
