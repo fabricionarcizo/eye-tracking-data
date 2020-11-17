10b5a2e181ed241021cab5b028e02e82a6b90894
Fabricio Batista Narcizo <fabn@itu.dk>
2020-11-17 21:09:08 +0100

    Simulated original polynomial with errors in degress

--- 03_scripts/00_python/create_error_bar.py
+++ 03_scripts/00_python/create_error_bar.py
@@ -33,41 +33,43 @@ for i, path in enumerate(files):
     errorY = []
     for csv in csvs:
         data = pd.read_csv(csv)
-        errorX.append(data.error_px_x.values)
-        errorY.append(data.error_px_y.values)
+        errorX.append(data.error_deg_x.values)
+        errorY.append(data.error_deg_y.values)
     errorX = np.asarray(errorX).reshape(-1)
     errorY = np.asarray(errorY).reshape(-1)
 
-    # Plot the X-axis error.
-    hist, bins = np.histogram(errorX, bins=21, density=True)
-    unityHist = hist / hist.sum()
+    # Calculate the X-axis histogram error.
+    hist, bins = np.histogram(errorX, bins=21)
+    print(i, hist)
+
+    # Histogram normalization.
     error = np.sqrt(hist) / hist.sum()
+    hist = hist / hist.sum()
     widths = bins[:-1] - bins[1:]
 
     # Calculate the Gaussian parameters.
-    peaks, properties = signal.find_peaks(unityHist, width=True)
+    peaks, properties = signal.find_peaks(hist, width=True)
 
     if len(peaks) > 1:
         model = gauss1Model + gauss2Model
         p1, p2, p3 = np.unique((properties["left_bases"], properties["right_bases"]))
-        params1 = gauss1Model.guess(unityHist[p1:p2], x=bins[p1:p2])
-        params2 = gauss2Model.guess(unityHist[p2:p3], x=bins[p2:p3])
+        params1 = gauss1Model.guess(hist[p1:p2], x=bins[p1:p2])
+        params2 = gauss2Model.guess(hist[p2:p3], x=bins[p2:p3])
         params = params1.update(params2)
     else:
         model = gauss1Model
-        params = model.guess(unityHist, x=bins[1:])
+        params = model.guess(hist, x=bins[1:])
 
     # Fit the Gaussian curve.
-    print(i, bins)
-    result = model.fit(unityHist, params, x=bins[1:])
+    result = model.fit(hist, params, x=bins[1:])
     xPlot = np.linspace(bins.min() * 1.10, bins.max() * 1.10, 1000)
     # comps = result.eval_components(x=xPlot) // Evaluate the individual Gaussian component
 
     # Plot the histogram.
-    plt.bar(bins[1:], unityHist, width=widths, edgecolor="k", alpha=0.35, color="k",
+    plt.bar(bins[1:], hist, width=widths, edgecolor="k", alpha=0.35, color="k",
             yerr=error, error_kw=dict(lw=1, capsize=2, capthick=1))
     plt.plot(xPlot, result.eval(x=xPlot), "k-", alpha=0.5, lw=2)
-    # plt.axvline(xPlot.mean(), color="k", alpha=0.5, linestyle='dashed', linewidth=1) # Plot the histogram mean
+    # plt.axvline(errorX.mean(), color="k", alpha=0.5, linestyle='dashed', linewidth=1) # Plot the histogram mean
 
     # Plot the Gaussian components.
     # plt.plot(xPlot, comps["g1_"], "k--", alpha=0.25, lw=2)
